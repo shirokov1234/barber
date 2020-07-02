5364ace8cec97cbeea43524a84ecc52559992567
Андрей Широков <andrejsirokov@MacBook-Pro-Andrej.local>
2020-07-02 19:22:15 +0300

    add more validation

--- app.rb
+++ app.rb
@@ -32,8 +32,19 @@ post "/visit" do
 
  if @username == ''
  	 @error = "Введите имя"
- 	return erb :visit
  end
+
+ if @phone == ''
+ 	 @error = "Введите номер телефона"
+ end
+
+ if @datetime == ''
+ 	 @error = "Не верная дата"
+ end
+
+ if @error != ''
+ 		return erb :visit
+ 	end
 
 erb "OK, username is #{@username},#{@phone},#{@datetime},#{@barber}, #{@color}"
 
